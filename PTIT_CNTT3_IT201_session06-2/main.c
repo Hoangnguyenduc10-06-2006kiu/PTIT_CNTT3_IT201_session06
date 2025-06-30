09281e11a0cc91324be7666f1c39cbe1f75bec3c
Hoangnguyenduc10-06-2006kiu <ducgay233@gmail.com>
2025-06-30 07:34:36 +0700

    add exercise06-3

--- PTIT_CNTT3_IT201_session06-2/main.c
+++ PTIT_CNTT3_IT201_session06-2/main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+git#include <stdio.h>
 
 int fibonacci(int n) {
     if (n == 0) {
--- PTIT_CNTT3_IT201_session06-4/main.c
+++ PTIT_CNTT3_IT201_session06-4/main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+void towerhn(int plate, char from, char to,char temp )
+{
+    //dieu kien dung
+    if (plate == 1)
+    {
+        printf("dia 1 di chuyen tu %c sang %c\n",from,to);
+        return;
+    }
+    //b1 chuyen n-1 dia tu form sang twmp
+    towerhn(plate-1,from,temp,to);
 
+    //b2 chuyen dia thu n tu form sang to
+    printf("dia %d di chuen tu %c sang %c\n",plate,from,to);
+    //b3 chuyen n-1 dai tu temp sang to
+    towerhn(plate-1,temp,to,from);
+
+}
 int main(void)
 {
-    printf("Hello, World!\n");
+    int plate;
+    printf("nhap vao so dia");
+    scanf("%d",&plate);
+    towerhn(plate, 'a', 'b', 'c');
     return 0;
 }
